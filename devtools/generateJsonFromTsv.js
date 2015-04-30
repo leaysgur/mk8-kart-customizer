a98b1c38c8b6306253be3ab55247060e6088274b
leader22 <y.sugiura.0316@gmail.com>
2015-04-30 23:03:36 +0900

    add master

---
+++ devtools/generateJsonFromTsv.js
@@ -0,0 +1,40 @@
+var target = process.argv[2];
+
+if (!target) { throw new Error('Target is undefined. [driver, body, tire, glider]') }
+
+var tmpl = `{
+    "id": "%s",
+    "name": "%s",
+    "speed": {
+        "ground":       "%s",
+        "water":        "%s",
+        "air":          "%s",
+        "anti-gravity": "%s"
+    },
+    "acceleration": "%s",
+    "weight": "%s",
+    "handling": {
+        "ground":       "%s",
+        "water":        "%s",
+        "air":          "%s",
+        "anti-gravity": "%s"
+    },
+    "traction": "%s",
+    "mini-turbo": "%s"
+}`;
+var outputStr = [];
+
+var fs = require('fs'),
+    rl = require('readline');
+var util = require('util');
+var inputStream   = fs.createReadStream(`${__dirname}/tsv/${target}.tsv`),
+    inputReadLine = rl.createInterface({ 'input': inputStream, 'output': {} });
+
+inputReadLine
+.on('line', (line) => {
+    var args = line.split('\t');
+    outputStr.push(util.format(tmpl, ...args));
+})
+.on('close', () => {
+    fs.writeFile(`${__dirname}/json/${target}.json`, '[' + outputStr + ']', () => {});
+});
