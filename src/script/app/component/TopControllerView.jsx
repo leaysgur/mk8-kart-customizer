24fc1ef07caae323aa2e6ce04f48780b0e4a593f
leader22 <y.sugiura.0316@gmail.com>
2015-05-03 16:35:53 +0900

    fix top page style

--- src/script/app/component/TopControllerView.jsx
+++ src/script/app/component/TopControllerView.jsx
@@ -7,6 +7,28 @@ define([
 
     let { Link } = Router;
 
+
+    const BtnView = React.createClass({
+        propTypes: {
+            label:     React.PropTypes.string.isRequired,
+            to:        React.PropTypes.string,
+            className: React.PropTypes.string,
+            primary:   React.PropTypes.bool
+        },
+        render() {
+            let className = this.props.className || '';
+            className += ' cBtn';
+            if (this.props.primary) { className += ' mPrimary'; }
+
+            return (
+                <Link className={className} to={this.props.to}>
+                    <p className="label">{this.props.label}</p>
+                </Link>
+            );
+        }
+    });
+
+
     const TopControllerView = React.createClass({
         render() {
             return (
@@ -14,8 +36,9 @@ define([
                     <h1 className="cMainImg">
                         <img className="img" src="dist/image/main.png" alt="MarioKart8 Kart Customizer" />
                     </h1>
-                    <Link to="custom">はじめる</Link>
-                    <Link to="about">このサービスについて</Link>
+
+                    <BtnView className="lTopBtn" label="はじめる" to="custom" primary />
+                    <BtnView label="このサービスについて" to="about" />
                 </section>
             );
         }
