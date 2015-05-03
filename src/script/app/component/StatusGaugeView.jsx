10682eae5a90c35795d9d1cc15837182c9351297
leader22 <y.sugiura.0316@gmail.com>
2015-05-03 21:11:38 +0900

    fix gauge style

--- src/script/app/component/StatusGaugeView.jsx
+++ src/script/app/component/StatusGaugeView.jsx
@@ -15,11 +15,23 @@ define([
             let className = this.props.className || '';
             className += ' cStatusGauge';
 
+            let maxValArr = __getEmptyArr(this.props.maxVal);
+
+            let gaugeStyle = {
+                width: `${((this.props.val/this.props.maxVal)*100).toFixed(2)}%`
+            };
+            let outerStyle = {
+                width: `${(100/this.props.maxVal).toFixed(2)}%`
+            };
+
             return (
                 <div className={className}>
                     <div className="label">{this.props.label}</div>
-                    <div className="outer">
-                        {this.props.val} / {this.props.maxVal}
+                    <div className="gaugeWrap" data-val={this.props.val}>
+                        <div className="gauge" style={gaugeStyle}></div>
+                        {maxValArr.map(() => {return (
+                        <span className="delimiter" style={outerStyle}></span>
+                        );})}
                     </div>
                 </div>
             );
@@ -27,4 +39,11 @@ define([
     });
 
     return StatusGaugeView;
+
+    function __getEmptyArr(len) {
+        let i = 0, l = len;
+        let retArr = [];
+        for (; i < l; i++) { retArr[retArr.length] = 1; }
+        return retArr;
+    }
 });
